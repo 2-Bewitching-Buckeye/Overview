66e4e3f367a023b2f4d64d357c5db221453e518a
Kevin Liu <kevinalexliu@gmail.com>
2021-03-25 10:07:12 -0700

    loader fix

--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const dotenv = require('dotenv');
-//loader verification
-const loaderToken = require('../loaderio-9562954a7d597633fdb1001e991dd007.txt')
+
 
 dotenv.config({
   path: './config.env',
@@ -16,7 +15,7 @@ app.use(express.json());
 app.use('/products', productsRouter)
 
 app.get('/loaderio-9562954a7d597633fdb1001e991dd007.txt', (req, res) => {
-  res.send(loaderToken)
+  res.send('loaderio-9562954a7d597633fdb1001e991dd007')
 })
 
 app.listen(port, () => {
