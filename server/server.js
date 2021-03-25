22fb9208d40bedfa1bafd8bf273f90890888b85f
Kevin Liu <kevinalexliu@gmail.com>
2021-03-25 10:02:39 -0700

    loader verify

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const dotenv = require('dotenv');
+//loader verification
+const loaderToken = require('../loaderio-9562954a7d597633fdb1001e991dd007.txt')
 
 dotenv.config({
   path: './config.env',
@@ -12,6 +14,10 @@ const productsRouter = require('./routes/productsRouter.js')
 app.use(express.json());
 
 app.use('/products', productsRouter)
+
+app.get('/loaderio-9562954a7d597633fdb1001e991dd007.txt', (req, res) => {
+  res.send(loaderToken)
+})
 
 app.listen(port, () => {
   console.log(`Connected to port: ${port}`)
